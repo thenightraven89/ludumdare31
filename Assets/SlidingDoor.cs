305ae727e6832235b38a9693cc4832050f9c7371
pulseman <bogdan.tana@gmail.com>
2014-12-06 12:51:40 +0200

    A sliding door is functional! Yay. Now for the rest of the game...

---
+++ Assets/SlidingDoor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingDoor : MonoBehaviour
+{
+    public bool isInitiallyOpen;
+
+    private bool isOpen;
+
+    public GameObject doorPiece;
+
+    public Transform openPosition;
+    public Transform closedPosition;
+
+    public float doorOperatingTime;
+
+    public void Start()
+    {
+        isOpen = isInitiallyOpen;
+        InitializeDoor(isOpen);
+    }
+
+    #region methods for handling door operations
+
+    public void OpenDoor()
+    {
+        LeanTween.moveLocalX(doorPiece, openPosition.localPosition.x, doorOperatingTime);
+    }
+
+    public void CloseDoor()
+    {
+        LeanTween.moveLocalX(doorPiece, closedPosition.localPosition.x, doorOperatingTime);
+    }
+
+    public void SetDoorOpen(bool value)
+    {
+        if (value)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    #endregion
+
+    public void InitializeDoor(bool isOpen)
+    {
+        if (isOpen)
+        {
+            doorPiece.transform.localPosition = openPosition.transform.localPosition;
+        }
+        else
+        {
+            doorPiece.transform.localPosition = closedPosition.transform.localPosition;
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return isOpen;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Walls"))
+        {
+            OpenDoor();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Walls"))
+        {
+            CloseDoor();
+        }
+    }
+}
